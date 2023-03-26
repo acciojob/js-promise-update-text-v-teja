2758dd920b17482df4551e4d79a6145553962820
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-03-26 22:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+sayHello().then((message) => {
+  outputElement.textContent = message;
+});
+
